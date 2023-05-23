ba88092aa5091df17d22b42704d868f4c07efc37
Maryam <morrryyyy20@gmail.com>
2023-05-23 10:50:11 -0700

    print a string

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Prints a character c
+ *
+ * @c - the character to be printed
+ *
+ * Return: Always 0 (Success);
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
