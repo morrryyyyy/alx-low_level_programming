6a94c67a8019a447edfa20f78bdcb13e6964be24
Maryam <morrryyyy20@gmail.com>
2023-05-29 01:36:13 -0700

    make file betty compliant

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,6 +2,7 @@
 
 /**
  * print_times_table - prints the n times table
+ * @n: the starting point of the times table
  */
 void print_times_table(int n)
 {
@@ -9,7 +10,7 @@ void print_times_table(int n)
 
 	if (n >= 0 && n < 15)
 	{
-	for (i = 0; i < n; i++ )
+	for (i = 0; i < n; i++)
 	{
 		j = 0;
 		while (j < 10)
