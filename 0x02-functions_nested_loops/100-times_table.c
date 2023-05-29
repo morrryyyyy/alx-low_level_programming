4560b219e61956fd9f4180b40bd1f6cbb535a6ad
Maryam <morrryyyy20@gmail.com>
2023-05-29 01:44:23 -0700

    print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,9 +10,9 @@ void print_times_table(int n)
 
 	if (n >= 0 && n < 15)
 	{
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			prod = i * j;
 			if (j == 0)
