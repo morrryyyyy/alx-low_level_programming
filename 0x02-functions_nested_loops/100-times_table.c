c0d826e5763dd42c55044b513c41d6f3c72fa2b3
Maryam <morrryyyy20@gmail.com>
2023-05-29 02:11:10 -0700

    print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,13 +26,21 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(prod + '0');
 			}
-			else if (prod >= 10)
+			else if (prod >= 10 && prod < 100)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
 			}
+			 else if (prod >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10) % 10) + '0');
+					_putchar((prod % 10) + '0');
+				}
 		}
 	_putchar('\n');
 	}
