96a18b581d6a95b0ab977a864b57153e654cdd47
Alabi Maryam <morrryyyy20@gmail.com>
2023-10-05 10:33:49 +0000

    print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "Programming is like building a multilingual puzzle";
+
+	puts(text);
+	return (0);
+}
