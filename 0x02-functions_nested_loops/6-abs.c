f7bb837e76a53f8584b3b714c5dc260efb7c95f0
Alabi Maryam <morrryyyy20@gmail.com>
2023-10-11 07:12:24 +0000

    prints the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a number
+ * @int: the number to be checked
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return(-n);
+	}
+	else
+	{
+		return (0);
+	}
+}
