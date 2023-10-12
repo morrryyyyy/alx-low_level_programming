2a91747994dc28059a858f9e7ddebdd833b75d7b
Alabi Maryam <morrryyyy20@gmail.com>
2023-10-12 04:13:39 +0000

    edit header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,10 @@ int _isalpha(int c);
 void print_alphabet(void);
 int print_sign(int n);
 int _abs(int n);
+int print_last_digit(int n);
+void print_to_98(int n);
+int add(int n, int i);
+void times_table(void);
+void jack_bauer(void);
+void print_times_table(int n);
 #endif
