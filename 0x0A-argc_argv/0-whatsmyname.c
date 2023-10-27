14ffce2a0d1d5d3ee0328c819d7af0c367142567
Alabi Maryam <morrryyyy20@gmail.com>
2023-10-27 22:02:04 +0000

    prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its own name
+ *@argc: one prameter
+ *@argv[]: another parameter
+ *
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
