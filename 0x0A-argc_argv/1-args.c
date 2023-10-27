2066326038036c289c38d1d1fd0dc5f50f52c202
Alabi Maryam <morrryyyy20@gmail.com>
2023-10-27 22:14:55 +0000

    print the number of command line arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments in the cli
+ *@argc: one argumnet
+ *argv: another argument
+ *
+ * Return: ALways 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	
+	printf("%d\n", argc);
+	return (0);
+}
