f044b7a277ba43aef2475ec592066b2467f82863
Maryam <morrryyyy20@gmail.com>
2024-01-11 09:33:11 +0100

    edit header file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,7 @@ void _puts_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _strlen_recursion(char *s);
+int check(int a, int b);
+int _sqrt_recursion(int n);
 
 #endif
