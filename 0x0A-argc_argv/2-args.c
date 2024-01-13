3cbf8d22d6cd7db40d25f0995010365ebeadbc5a
Maryam <morrryyyy20@gmail.com>
2024-01-13 12:17:28 +0100

    print each argument followed by a new line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the arguments passed to it
+ *@argc: the number of arguments
+ *@argv: the arguments
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(int argc, char **argv)
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		puts(argv[i]);
+	}
+	return (0);
+}
