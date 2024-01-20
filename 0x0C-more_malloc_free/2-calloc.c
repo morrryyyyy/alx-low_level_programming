bbc8d16f4b07563805bfc3971b9b62e08e8b23cb
Maryam <morrryyyy20@gmail.com>
2024-01-20 09:48:11 +0100

    malloc is hard

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ *@nmemb: the number of elements in the array
+ *@size: the size of each element
+ *
+ * Return: a void pointer
+ **/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(size * nmemb);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
