812b9cb5f2e111e97893417e870a8020dc75c1b7
Maryam <morrryyyy20@gmail.com>
2024-01-23 11:25:33 +0100

    update header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,5 +17,6 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
+void free_dog(dog_t *d);
 
 #endif /* _DOG_H_*/
