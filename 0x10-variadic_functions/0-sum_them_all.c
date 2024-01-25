d174633dcd710a14818ca62305c907686561ba7c
Maryam <morrryyyy21@gmail.com>
2024-01-25 13:34:17 +0100

    add all arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - adds all the arguments given to it
+ *@n: the number of arguments
+ *
+ * Return: the sum of the arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+	int x;
+	va_list args;
+
+	sum = 0;
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(args, int);
+		sum += x;
+	}
+	va_end(args);
+	return (sum);
+}
