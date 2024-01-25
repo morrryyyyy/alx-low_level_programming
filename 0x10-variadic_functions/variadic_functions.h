b9000ca04f80939f01079accd82296499c90a21e
Maryam <morrryyyy21@gmail.com>
2024-01-25 13:34:07 +0100

    create header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *seperator, const unsigned int n, ...);
+void print_strings(const char *seperator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */
