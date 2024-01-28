a40e8ec306c3b9ed75af13b3264da2d1c21670b3
Maryam <morrryyyy21@gmail.com>
2024-01-28 08:22:49 +0100

    edit header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,8 +2,10 @@
 #define _FUNCTION_POINTERS_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* _FUNCTION_POINTERS_H_ */
