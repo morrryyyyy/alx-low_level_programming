053dae4db76fd08a32f20bcaec05d8a7a69c877f
Maryam <morrryyyy21@gmail.com>
2024-03-02 17:15:15 +0100

    add a new node to the list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list
+ *@head: a pointer to the head
+ *@str: the string to be stored in the new node
+ *
+ * Return: the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *new_str;
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new_str = strdup(str);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new->str = new_str;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
