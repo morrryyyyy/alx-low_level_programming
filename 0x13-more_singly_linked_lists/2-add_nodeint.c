91d83407c41d839189de0e3cdd669d9268d703f6
Maryam <morrryyyy21@gmail.com>
2024-03-08 07:50:31 +0100

    add a new node to a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,6 +16,11 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (new == NULL)
 		return (NULL);
+	if (new_num == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
 	new->n = new_num;
 	new->next = *head;
