36543aab42a90aad4d0db9cc638fb0d52dd1e55c
Maryam <morrryyyy21@gmail.com>
2024-03-23 16:58:19 +0100

    get the nth node of a linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -15,6 +15,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	count = 0;
 	current = head;
 
+	if (head = NULL)
+		return (NULL);
 	while (current->next != NULL && count < index)
 	{
 		count++;
