5d1af1decba172ebd30ea30d76cde774ee690ff9
Maryam <morrryyyy21@gmail.com>
2024-03-23 17:04:30 +0100

    sum all the data of a linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - gets the sum of all the dsta of a inked list
+ * @head: the head of the linked list
+ *
+ * Return: the sum of the data
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+	int sum;
+
+	sum = 0;
+	current = head;
+
+	while (current->next != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
